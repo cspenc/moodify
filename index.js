0c48863aab8a71806fbc8b1d95fbc88800ca7a51
Amanda Steckler <amandajsteckler@gmail.com>
2016-11-14 12:33:35 -0500

    slideshow mostly working

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ $(document).ready(function() {
     event.preventDefault()
     getSong()
     getPhotos()
+    $('#carousel-example-generic').carousel({
+      interval: 2000
+    })
 
     $(".overlay, .overlay-message").show();
 
     $(".overlay-message").click(function() {
+        $('.carousel-inner').empty();
         $(".overlay, .overlay-message").hide();
     });
 
@@ -20,30 +24,24 @@ function getPhotos() {
   } else {
     var tags = store.artists[store.artists.length - 1].name
   }
+
   $.ajax({
       method: "GET",
       url: `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=81501bbe9c20b4cd761970acfb46108f&tags=${tags}&safe_search=1&format=json&nojsoncallback=1`
     }).done(function(data) {
      var photosArray = data.photos.photo
+     debugger
      for (let i = 0; i < 15; i++) {
        store.photos.push(photosArray[Math.floor(Math.random() * photosArray.length)])
      }
-     var newArray = store.photos.slice(store.photos.length - 15, store.photos.length - 1)
-     $('#slideshow').empty()
+     var newArray = store.photos.slice(store.photos.length - 15, store.photos.length - 2)
+     $('.carousel-inner').empty()
+     var firstPhoto = store.photos[store.photos.length-1]
+     $('.carousel-inner').append(`<div class="item active"><img src="https://farm${firstPhoto.farm}.staticflickr.com/${firstPhoto.server}/${firstPhoto.id}_${firstPhoto.secret}.jpg"/></div>`)
      newArray.forEach(function(photo) {
-       $('#slideshow').append(`<img src="https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg"/>`)
+       $('.carousel-inner').append(`<div class="item"><img src="https://farm${photo.farm}.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}.jpg"/></div>`)
      })
   })
-
-  // $(function(){
-  //   $('.fadein img:gt(0)').hide();
-  //   setInterval(function(){
-  //     $('.fadein :first-child').fadeOut()
-  //        .next('img').fadeIn()
-  //        .end().appendTo('.fadein');},
-  //     6000);
-// });
-
 }
 
 function getSong() {
@@ -58,6 +56,9 @@ function getSong() {
         method: "GET",
         url: `https://api.spotify.com/v1/search?q=track:${song}+artist:${artist}&type=track`
       }).done(function(data) {
+          if (data.tracks.items.length < 1) {
+            $('#spotify').append('Cannot find track. You fucked up. Please check your song and/or artist.')
+          }
           let id = data.tracks.items[0].id
           $('#spotify').append(`<iframe src="https://embed.spotify.com/?uri=spotify:track:${id}&theme=white&view=coverart" width="300" height="100" frameborder="0" allowtransparency="true"></iframe>`)
         })
@@ -78,6 +79,8 @@ function getSong() {
           $('#spotify').append(`<h5>You gave me no song title, so here's a random track that fits your artist, "${artist}":</h5><br><iframe src="https://embed.spotify.com/?uri=spotify:track:${id}&theme=white&view=coverart" width="300" height="100" frameborder="0" allowtransparency="true"></iframe>`)
         })
   }
+
+
 
 
 }
